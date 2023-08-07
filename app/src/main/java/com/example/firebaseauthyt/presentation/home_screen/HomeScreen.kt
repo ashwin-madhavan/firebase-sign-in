fea27f633f6386619ef175b81a4caccece2ef9c6
Ashwin Madhavan <ashwinmadhavan12@gmail.com>
2023-08-07 16:53:43 -0700

    HomeScreen ui updates

--- app/src/main/java/com/example/firebaseauthyt/presentation/home_screen/HomeScreen.kt
+++ app/src/main/java/com/example/firebaseauthyt/presentation/home_screen/HomeScreen.kt
@@ -1,12 +1,15 @@
 package com.example.firebaseauthyt.presentation.home_screen
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -21,10 +24,13 @@ import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import com.example.firebaseauthyt.model.MovieReview
 import com.example.firebaseauthyt.presentation.DatabaseViewModel
@@ -50,29 +56,55 @@ fun HomeScreen(
         },
         floatingActionButtonPosition = FabPosition.End,
         content = {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(8.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.Top
             ) {
-                Button(onClick = onManageFriendClicked) {
-                    Text(text = "Manage Friends")
-                }
-                Button(onClick = onGroupChatsClicked) {
-                    Text(text = "Group Chats")
-                }
-                LazyColumn(
-                    contentPadding = PaddingValues(
-                        vertical = 8.dp,
-                        horizontal = 8.dp
-                    )
+                Column(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .background(Color.Gray)
+                        .weight(0.2f)
                 ) {
-                    items(viewModel.curUserMovieReviewListState.value) { movieReview ->
-                        MovieReviewItem(movieReview, onItemClick)
+                    Button(onClick = {}) {
+                        Image(
+                            imageVector = Icons.Filled.Home,
+                            contentDescription = "Rating Icon",
+                        )
+                    }
+                    Button(onClick = onManageFriendClicked) {
+                        Text(text = "Friends")
+                    }
+                    Button(onClick = onGroupChatsClicked) {
+                        Text(text = "Group Chats")
+                    }
+                }
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(8.dp)
+                        .weight(0.8f), // Ensure the column occupies remaining space
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "My Movie History",
+                        style = MaterialTheme.typography.h5,
+                        textDecoration = TextDecoration.Underline
+                    )
+                    LazyColumn(
+                        contentPadding = PaddingValues(
+                            vertical = 8.dp,
+                            horizontal = 8.dp
+                        )
+                    ) {
+                        items(viewModel.curUserMovieReviewListState.value) { movieReview ->
+                            MovieReviewItem(movieReview, onItemClick)
+                        }
                     }
                 }
             }
+
         }
     )
 }
