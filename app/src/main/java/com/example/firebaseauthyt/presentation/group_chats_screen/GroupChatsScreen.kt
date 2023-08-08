a3b794c1dabc454579b450b0b19d56f872559c5f
Ashwin Madhavan <ashwinmadhavan12@gmail.com>
2023-08-08 14:49:51 -0700

    Ui updates

--- app/src/main/java/com/example/firebaseauthyt/presentation/group_chats_screen/GroupChatsScreen.kt
+++ app/src/main/java/com/example/firebaseauthyt/presentation/group_chats_screen/GroupChatsScreen.kt
@@ -1,24 +1,39 @@
 package com.example.firebaseauthyt.presentation.group_chats_screen
 
-import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Button
 import androidx.compose.material.Card
+import androidx.compose.material.Checkbox
+import androidx.compose.material.FabPosition
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.firebaseauthyt.model.Group
-import com.example.firebaseauthyt.model.User
-import com.example.firebaseauthyt.presentation.home_screen.MovieReviewItem
 
 @Composable
 fun GroupChatsScreen(onItemClick: (Long) -> Unit) {
@@ -26,19 +41,67 @@ fun GroupChatsScreen(onItemClick: (Long) -> Unit) {
     val thisCurUser by viewModel.userLiveData.observeAsState()
     val thisCurUserGroups by viewModel.groupLiveData.observeAsState()
 
-    Column {
-        val userName = thisCurUser?.name ?: "Loading Name..."
-        LazyColumn(
-            contentPadding = PaddingValues(
-                vertical = 8.dp,
-                horizontal = 8.dp
-            )
-        ) {
-            items(thisCurUserGroups ?: emptyList()) { item ->
-                GroupCard(group = item, onItemClick)
+    var showDialog by remember { mutableStateOf(false) }
+
+    if (showDialog) {
+        AlertDialog(
+            onDismissRequest = { showDialog = false },
+            title = { Text("Create Group Chat") },
+            text = {
+                Column {
+                    val options = listOf("Option 1", "Option 2", "Option 3")
+                    CreateGroupForm(options = options)
+                }
+            },
+            confirmButton = {
+                Button(
+                    onClick = {
+                        showDialog = false
+                    }
+                ) {
+                    Text("Create Group")
+                }
+            },
+            dismissButton = {
+                Button(
+                    onClick = { showDialog = false }
+                ) {
+                    Text("Cancel")
+                }
+            }
+        )
+    }
+
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { showDialog = true },
+            ) {
+                Icon(Icons.Default.Add, contentDescription = "Add")
+            }
+        },
+        floatingActionButtonPosition = FabPosition.End,
+        content = {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                val userName = thisCurUser?.name ?: "Loading Name..."
+                Text(
+                    text = "Group Chats",
+                    style = MaterialTheme.typography.h5,
+                    textDecoration = TextDecoration.Underline
+                )
+                LazyColumn(
+                    contentPadding = PaddingValues(
+                        vertical = 8.dp,
+                        horizontal = 8.dp
+                    )
+                ) {
+                    items(thisCurUserGroups ?: emptyList()) { item ->
+                        GroupCard(group = item, onItemClick)
+                    }
+                }
             }
         }
-    }
+    )
 }
 
 @Composable
@@ -58,3 +121,40 @@ fun GroupCard(group: Group, onItemClick: (Long) -> Unit) {
         }
     }
 }
+
+@Composable
+fun CreateGroupForm(options: List<String>) {
+    val selectedOptions = remember { mutableStateListOf<String>() }
+    var newTitle by remember { mutableStateOf("") }
+
+    Column {
+        TextField(
+            value = newTitle,
+            onValueChange = { newTitle = it },
+            label = { Text("Group Name") }
+        )
+
+        Text(text = "Select Friends:")
+
+        options.forEach { option ->
+            Checkbox(
+                checked = selectedOptions.contains(option),
+                onCheckedChange = { isChecked ->
+                    if (isChecked) {
+                        selectedOptions.add(option)
+                    } else {
+                        selectedOptions.remove(option)
+                    }
+                }
+            )
+            Text(text = option)
+        }
+
+        Text("Selected Friends: ${selectedOptions.joinToString(", ")}")
+    }
+}
+
+
+
+
+
